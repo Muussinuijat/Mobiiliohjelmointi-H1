aaa21d08bad4cad2919248ef49cdd26a2d58cc2b
Muussinuijat <t3lihe00@students.oamk.fi>
2025-11-05 12:11:52 +0200

    Appi valmis

--- H1/App.tsx
+++ H1/App.tsx
@@ -1,20 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState("");
+  const [lowerLimit, setLowerLimit] = useState(0);
+  const [upperLimit, setUpperLimit] = useState(0);
+
+  const calculateHeartRate = (input: string) => {
+    setAge(input);
+    const inputAge = parseInt(input);
+    if (isNaN(inputAge) || inputAge <= 0) {
+      setLowerLimit(0);
+      setUpperLimit(0);
+      return;
+    }
+    const lower = Math.round((220 - inputAge) * 0.65);
+    const upper = Math.round((220 - inputAge) * 0.85);
+    setLowerLimit(lower);
+    setUpperLimit(upper);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Heart Rate Limits Calculator</Text>
+      <Text style={styles.label}>Enter your Age:</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={age}
+        onChangeText={calculateHeartRate}
+        placeholder="Empty"
+      />
+      <View style={styles.results}>
+        <Text style={styles.resultText}>Lower Limit: {lowerLimit} bpm</Text>
+        <Text style={styles.resultText}>Upper Limit: {upperLimit} bpm</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  results: {
+  
+  },
+  resultText: {
+    fontSize: 18,
+    marginTop: 5,
   },
 });
